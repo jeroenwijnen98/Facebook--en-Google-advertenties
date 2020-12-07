e427ddf1cc29e4074317f55611f833b01b6715a2
jeroenwijnen98 <74405462+jeroenwijnen98@users.noreply.github.com>
2020-12-07 11:19:59 +0100

    Update Packages.R

--- Packages.R
+++ Packages.R
@@ -1,17 +1,14 @@
-library(ggplot2) 
+
 library(tidyr) #voor herrankschikken dataset
 library(devtools) #idem
-library(pdftools) #scraping pdf
 library(dplyr) #for merging
 library(openxlsx)
 library(gsubfn)
-library(cat)
 library(base)
 library(stringr)
 library(anchors)
 library(data.table)
 library(stringr)
-library(rvest) #scraping
 library(rjson)
 library(gtools)
 library(xml2) #scraping
@@ -23,8 +20,6 @@ library(janitor) #voor maken column totals
 library(fastDummies)
 library(pmdplyr)
 library(sjmisc)
-library(googlesheets4)
-library(webshot)
 library(wdman)
 library(zoo)
 library(schoolmath)
