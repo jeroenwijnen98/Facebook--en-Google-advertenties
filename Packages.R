1227dfaaa97eb419d5134c87f1529df80f15f34b
jeroenwijnen98 <74405462+jeroenwijnen98@users.noreply.github.com>
2020-12-07 11:20:35 +0100

    Update Packages.R

--- Packages.R
+++ Packages.R
@@ -4,8 +4,6 @@ library(devtools) #idem
 library(dplyr) #for merging
 library(openxlsx)
 library(gsubfn)
-library(base)
-library(stringr)
 library(anchors)
 library(data.table)
 library(stringr)
@@ -14,7 +12,6 @@ library(gtools)
 library(xml2) #scraping
 library(RSelenium) #scraping
 library(httr) #scraping
-library(Rfacebook)
 library(XML)
 library(janitor) #voor maken column totals
 library(fastDummies)
